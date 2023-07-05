1c8b319e7d62106b424425b4db8839ada39e5695
Mohd Dilshad <mohddilshad.211it042@nitk.edu.in>
2023-07-05 18:13:17 +0530

    setup of customising screen

---
+++ client/src/pages/Customizer.jsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from "react"
+import { useSnapshot } from "valtio";
+import { motion, AnimatePresence } from 'framer-motion';
+import state from "../store";
+import config from '../config/config';
+import { download } from '../assets'
+import { downloadCanvasToImage, reader } from '../config/helpers';
+import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
+import { fadeAnimation, slideAnimation } from '../config/motion';
+
+import {
+  CustomButton
+  , AIPicker
+  , ColorPicker
+  , FilePicker
+  , Tab
+} from '../components';
+
+const Customizer = () => {
+  const snap = useSnapshot(state);
+  return (
+    <AnimatePresence>
+      {!snap.intro && (
+        <>
+
+        {/************* Side Tabs for Customisation ****************/}
+          <motion.div
+          key = "custom"
+          className="absolute top-0 left-0 z-10" {...slideAnimation('left')}>
+
+          <div className="flex item-center min-h-screen">
+            <div className="editortabs-container tabs">
+            {EditorTabs.map((tab)=>(<Tab 
+              key={tab.name}
+              tab = {tab}
+              handleClick = {() => {}}
+            />))}
+
+            </div>
+          </div>
+
+          </motion.div>
+
+
+          {/************* Go Back Button *************/}
+          <motion.div
+          className="absolute z-10 top-5 right-5 " {...fadeAnimation}>
+            <CustomButton
+            type="filled"
+            title="Go Back"
+            handleClick={()=>{state.intro = true}}
+              customStyles='w-fit px-4 py-2.5 font-bold text-sm'
+            />
+
+          </motion.div>
+
+            
+            <motion.div className="filtertabs-container" {...slideAnimation('up')}>
+            
+            {FilterTabs.map((tab)=>(<Tab 
+              key={tab.name}
+              tab = {tab}
+              isFilterTab
+              isActiveTab=""
+              handleClick = {() => {}}
+            />))}
+
+            
+            </motion.div>
+
+
+        </>
+      )}
+    </AnimatePresence>
+  )
+}
+
+export default Customizer
