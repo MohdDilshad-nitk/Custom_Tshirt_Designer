bb23c9c2a71a41b6a5cf2ad801f84b99c3828f91
Mohd Dilshad <mohddilshad.211it042@nitk.edu.in>
2023-07-07 17:01:57 +0530

    AI Picker - Calling the REST API from Backend

--- client/src/components/AIPicker.jsx
+++ client/src/components/AIPicker.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import CustomButton from './CustomButton'
 
-const AIPicker = () => {
+const AIPicker = ({prompt,setPrompt,generatingImg,handleSubmit}) => {
   return (
-    <div>AIPicker</div>
+    <div className='aipicker-container'>
+      <textarea className='aipicker-textarea' placeholder='Ask AI' rows={5} value={prompt}
+        onChange={(e)=>{setPrompt(e.target.value)}}
+      />
+
+      <div 
+      className='flex flex-wrap gap-3'>
+        {generatingImg ? (
+          <CustomButton
+          type='outline'
+          title='Asking AI'
+          handleClick={()=> handleSubmit('logo')}
+          customStyles='text-xs' />
+        ) : (
+          <>
+            <CustomButton 
+              type="outline"
+              title="AI Logo"
+              handleClick={() => handleSubmit('logo')}
+              customStyles="text-xs"
+            />
+
+            <CustomButton 
+              type="filled"
+              title="AI Full"
+              handleClick={() => handleSubmit('full')}
+              customStyles="text-xs"
+            />
+          </>
+        )}
+      </div>
+    </div>
   )
 }
 
