86c9a4fbdf5bb1ce59769c745c164c2631dbc4d3
Mohd Dilshad <mohddilshad.211it042@nitk.edu.in>
2023-07-07 17:03:54 +0530

    Add Outline Button ,Made TextColor contrasting

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import state from '../store'
 import { useSnapshot } from 'valtio';
+import { getContrastingColor } from '../config/helpers';
+
 const CustomButton = ({type ,title,customStyles,handleClick}) => {
 
     const snap = useSnapshot(state);
 
     const generateStyle = (type) => {
-        switch (type) {
-            case "filled":
-                return {
-                    backgroundColor: snap.color,
-                    color: "#fff",
-                }
+        if(type === 'filled') {
+            return {
+              backgroundColor: snap.color,
+              color: getContrastingColor(snap.color)
             }
-        }
+          } else if(type === "outline") {
+            return {
+              borderWidth: '1px',
+              borderColor: snap.color,
+              color: snap.color
+            }
+          }}
 
   return (
     <button
