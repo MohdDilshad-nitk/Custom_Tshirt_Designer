20739dbb5d0e82b1c5ee19046a1c3e504aca8dae
Mohd Dilshad <mohddilshad.211it042@nitk.edu.in>
2023-07-07 17:02:28 +0530

    Color Picker Using SketchPicker from react-color

--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { SketchPicker } from 'react-color';
+import { useSnapshot } from 'valtio';
+
+import state from '../store';
+import { color } from 'framer-motion';
 
 const ColorPicker = () => {
+  const snap = useSnapshot(state);
   return (
-    <div>ColorPicker</div>
+    <div
+      className='absolute left-full ml-3'>
+      <SketchPicker
+        color={snap.color}
+        disableAlpha
+        presetColors={[
+          '#000000',
+          '#FFFFFF',
+          '#f0f8ff',
+          '#faebd7',
+          '#00ffff',
+          '#7fffd4',
+          '#f0ffff',
+          '#f5f5dc',
+          '#ffe4c4',
+          '#ffebcd',
+
+        ]}
+        onChange={(color) => state.color = color.hex}
+      />
+    </div>
   )
 }
 
